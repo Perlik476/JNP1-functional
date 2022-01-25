b52e4741923e885f89469424339c871c226acd2b
Perlik <mp429581@students.mimuw.edu.pl>
2022-01-25 14:56:47 +0100

    identity, compose, constructors, range_over and push_back implemented

--- src/tri_list.h
+++ src/tri_list.h
@@ -21,8 +21,8 @@ T identity(T t) {
     return t;
 }
 
-template <typename T, modifier<T> M1, modifier<T> M2>
-modifier<T> auto compose (M1 f1, M2 f2) {
+template <typename T, modifier<T> F1, modifier<T> F2>
+modifier<T> auto compose (F1 f1, F2 f2) {
     return [=](T t) {
         return f1(f2(t));
     };
@@ -34,18 +34,24 @@ class tri_list {
 
     std::vector<variant> list;
 
-    std::function<T1(T1)> T1_modifier = identity<T1>();
-    std::function<T2(T2)> T2_modifier = identity<T2>();
-    std::function<T3(T3)> T3_modifier = identity<T3>();
+    std::function<T1(T1)> T1_modifier = identity<T1>;
+    std::function<T2(T2)> T2_modifier = identity<T2>;
+    std::function<T3(T3)> T3_modifier = identity<T3>;
+
+public:
+
+    tri_list() = default;
+
+    tri_list(std::initializer_list<variant> list) : list(list) {}
 
     template<class T>
     auto range_over() {
         auto result = list
-            | std::views::filter([](variant el) {
-                return std::holds_alternative<T>(el);
+            | std::views::filter([](variant element) {
+                return std::holds_alternative<T>(element);
             })
-            | std::views::transform([](variant el) {
-                return std::get<T>(el);
+            | std::views::transform([](variant element) {
+                return std::get<T>(element);
             });
 
         if constexpr (std::is_same<T, T1>::value) {
@@ -57,6 +63,21 @@ class tri_list {
         else {
             return result | std::views::transform(T3_modifier);
         }
+    }
+
+    template <typename T>
+    void push_back(const T& t) {
+        list.push_back(t);
+    }
+
+    template <typename T>
+    void reset() {
+
+    }
+
+    template <typename T, modifier<T> F>
+    void modify_only(F f = F{}) {
+
     }
 };
 
